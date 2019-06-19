d27b4a90e15718a9415f3468157637f48a4303f5
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-06-19 18:01:47 -0500

    Added if statement

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
     host: "alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
@@ -11,7 +11,7 @@ var connection;
     password: "bn5i68jtg9y38mnm",
     database: "o935s3w4he5ojtwr"
   });
-// }
+}
 
 
 connection.connect(function (err) {
