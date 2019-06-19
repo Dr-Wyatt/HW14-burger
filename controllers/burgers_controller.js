dfdf385351e664b5a5873fde59b06f93c75f8b6b
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-06-18 20:24:46 -0500

    Added functionality to create, devour and re-order

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,39 +12,33 @@ router.get("/", function (req, res) {
     });
 });
 
-// router.post("/api/cats", function (req, res) {
-//     cat.create([
-//         "name", "sleepy"
-//     ], [
-//             req.body.name, req.body.sleepy
-//         ], function (result) {
-//             // Send back the ID of the new quote
-//             res.json({ id: result.insertId });
-//         });
-// });
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
-// router.put("/api/cats/:id", function (req, res) {
-//     var condition = "id = " + req.params.id;
-
-//     console.log("condition", condition);
-
-//     cat.update({
-//         sleepy: req.body.sleepy
-//     }, condition, function (result) {
-//         if (result.changedRows == 0) {
-//             // If no rows were changed, then the ID must not exist, so 404
-//             return res.status(404).end();
-//         } else {
-//             res.status(200).end();
-//         }
-//     });
-// });
-
-// router.delete("/api/cats/:id", function (req, res) {
-//     let id = "id = " + req.param.id;
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
-// })
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+            req.body.name, req.body.devoured
+        ], function (result) {
+            // Send back the ID of the new quote
+            res.json({ id: result.insertId });
+        });
+});
 
 
 
---
+++ public/assets/js/burgers_buttons.js
@@ -0,0 +1,51 @@
+$(function () {
+    $(".devour-burger").on("click", function (event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+      var flippedDevoured;
+      if (newDevoured == false){
+        flippedDevoured = true;
+      } 
+      if (newDevoured == true){
+        flippedDevoured = false;
+      }
+      console.log(flippedDevoured);
+      var newDevouredState = {
+        devoured: flippedDevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function () {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+    $("#add-new-burger").on("submit", function (event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      
+      var newBurger = {
+        name: $("#new-burger").val().trim(),
+        devoured: 0
+      };
+      console.log(newBurger.burger_name);
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function () {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
