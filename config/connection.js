0ce6b7a7c0b3803b3f5792d3eaa0582e5fe49d69
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-06-19 21:04:04 -0500

    adjusted if statement

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "root",
     port: 3306,
-    user: "vo9iogfs0fik3t6w",
-    password: "bn5i68jtg9y38mnm",
-    database: "o935s3w4he5ojtwr"
+    user: "localhost",
+    password: "Deadbeef12345",
+    database: "burgers"
   });
 }
 
